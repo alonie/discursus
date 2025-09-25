684ba071aed4022f6623e1e6c8d728bd2ab56d9c
alonie <andrew.lonie@gmail.com>
2025-09-25 13:02:05 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ demo = gr.Interface(fn=hello, inputs="text", outputs="text", title="Hello World"
 if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0", 
-        server_port=int(os.environ.get("PORT", 7860))
+        server_port=int(os.environ.get("PORT", 7860)),
+        share=True
     )
